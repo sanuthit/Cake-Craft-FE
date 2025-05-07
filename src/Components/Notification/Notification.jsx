40413eb34bbdbf8d30bc7c6499d134d381465962
Gavindu Anushka <gavinduanushka27@gmail.com>
2025-05-07 20:44:53 +0530

    Added notification buttons

--- src/Components/Notification/Notification.jsx
+++ src/Components/Notification/Notification.jsx
@@ -225,7 +225,7 @@ const Notification = () => {
                                                             className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                                                         >
                                                             Mark as read
-                                                        </button>
+                                                        </button> //added notification buttons //
                                                     )}
                                                     <button
                                                         onClick={() => handleDelete(item.id)}
